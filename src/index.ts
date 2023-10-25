1763ddc14305b3217d31013183fc46a69a07edcf
VN Phạm <nam.pv1805@gmail.com>
2023-10-26 00:18:23 +0700

    Update export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import * as CommitteeMember from './CommitteeMember';
 import * as DynamicArray from './DynamicArray';
 import * as ElgamalECC from './Elgamal';
 
-// export { CommitteeMember, DynamicArray, ElgamalECC };
-module.exports = {
-    CommitteeMember: CommitteeMember,
-    DynamicArray: DynamicArray,
-    ElgamalECC: ElgamalECC,
-};
+export { CommitteeMember, DynamicArray, ElgamalECC };
+// module.exports = {
+//     CommitteeMember: CommitteeMember,
+//     DynamicArray: DynamicArray,
+//     ElgamalECC: ElgamalECC,
+// };
